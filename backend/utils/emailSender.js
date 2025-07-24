0e555ecded3bac9cae050c44e660fd79ccb211b6
Rajkumar Yogi <rajkumar6777y@gmail.com>
2025-07-24 10:05:19 +0530

    Added email sender

---
+++ backend/utils/emailSender.js
@@ -0,0 +1,23 @@
+const nodemailer = require('nodemailer');
+require('dotenv').config();
+
+const transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: process.env.EMAIL_USERNAME,
+    pass: process.env.EMAIL_PASSWORD
+  }
+});
+
+const sendEmail = async (options) => {
+  const mailOptions = {
+    from: 'Car Service <noreply@carservice.com>',
+    to: options.email,
+    subject: options.subject,
+    text: options.message
+  };
+
+  await transporter.sendMail(mailOptions);
+};
+
+module.exports = sendEmail;
