7b2f48374545cfb73a3d93046103b5d229174429
Rajkumar Yogi <rajkumar6777y@gmail.com>
2025-07-24 10:06:15 +0530

    Added io socket

---
+++ backend/utils/socket.js
@@ -0,0 +1,13 @@
+module.exports = (io) => {
+  io.on('connection', (socket) => {
+    console.log('New client connected');
+    
+    socket.on('newBooking', (booking) => {
+      io.emit('adminNotification', booking);
+    });
+    
+    socket.on('disconnect', () => {
+      console.log('Client disconnected');
+    });
+  });
+};
