ceaa2ac81c456b4c35390d7fb0b2156c444fcab0
Rajkumar Yogi <rajkumar6777y@gmail.com>
2025-07-25 12:05:53 +0530

    Logo added to navbar

--- frontend/src/components/common/Navbar.jsx
+++ frontend/src/components/common/Navbar.jsx
@@ -1,5 +1,6 @@
 import { Link } from 'react-router-dom'
 import { useAuth } from '../../contexts/AuthContext'
+import logo from '../../assets/logo.png'
 
 export default function Navbar() {
   const { user, logout } = useAuth()
@@ -7,6 +8,9 @@ export default function Navbar() {
   return (
     <nav className="bg-blue-600 text-white shadow">
       <div className="container mx-auto px-4 py-3 flex justify-between items-center">
+      <Link to="/">
+        <img src={logo} alt="Logo" className="w-20 h-20 border" />
+      </Link>
         <Link to="/" className="text-xl font-bold">Car Service</Link>
         
         <div className="flex items-center space-x-4">
