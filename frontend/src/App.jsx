d8d83fb8b9fc19a4d74e9dbcc68444a9a9ede8d9
Rajkumar Yogi <rajkumar6777y@gmail.com>
2025-07-25 14:19:38 +0530

    route add for adminUserDetails

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,30 +1,32 @@
-import { Routes, Route } from 'react-router-dom'
-import { AuthProvider } from './contexts/AuthContext'
-import { ToastContainer } from 'react-toastify'
-import 'react-toastify/dist/ReactToastify.css'
-import Home from './pages/Home'
-import Dashboard from './pages/Dashboard'
-import Admin from './pages/Admin'
-import AdminUsers from './components/admin/AdminUsers'
-import AdminBookings from './components/admin/AdminBookings'
-import AdminServices from './components/admin/AdminServices'
-import Services from './pages/Services'
-import Bookings from './pages/Bookings'
-import Cart from './pages/Cart'
-import Navbar from './components/common/Navbar'
-import Footer from './components/common/Footer'
-import PrivateRoute from './components/auth/PrivateRoute'
-import AdminRoute from './components/auth/AdminRoute'
-import Login from './pages/Login'
-import Register from './pages/Register'
-import AdminLogin from './pages/AdminLogin'
+import { Routes, Route } from "react-router-dom";
+import { AuthProvider } from "./contexts/AuthContext";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import Home from "./pages/Home";
+import Dashboard from "./pages/Dashboard";
+import Admin from "./pages/Admin";
+import AdminUsers from "./components/admin/AdminUsers";
+import AdminBookings from "./components/admin/AdminBookings";
+import AdminServices from "./components/admin/AdminServices";
+import Services from "./pages/Services";
+import Bookings from "./pages/Bookings";
+import Cart from "./pages/Cart";
+import Navbar from "./components/common/Navbar";
+import Footer from "./components/common/Footer";
+import PrivateRoute from "./components/auth/PrivateRoute";
+import AdminRoute from "./components/auth/AdminRoute";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import AdminLogin from "./pages/AdminLogin";
+import AdminUserDetails from "./components/admin/AdminUserDetails";
+
 
 function App() {
   return (
     <AuthProvider>
       <div className="flex flex-col min-h-screen">
         {/* Toast Container */}
-        <ToastContainer 
+        <ToastContainer
           position="top-right"
           autoClose={5000}
           hideProgressBar={false}
@@ -36,15 +38,15 @@ function App() {
           pauseOnHover
           theme="light"
         />
-        
+
         {/* Conditional rendering for Navbar */}
         <Routes>
           {/* Routes without Navbar */}
-            <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
           <Route path="/admin-login" element={null} />
           <Route path="/admin/*" element={null} />
-          
+
           {/* All other routes will render Navbar */}
           <Route path="*" element={<Navbar />} />
         </Routes>
@@ -55,51 +57,80 @@ function App() {
             <Route path="/" element={<Home />} />
             <Route path="/services" element={<Services />} />
             <Route path="/admin-login" element={<AdminLogin />} />
-            
+
             {/* Protected User Routes */}
-            <Route path="/dashboard" element={
-              <PrivateRoute>
-                <Dashboard />
-              </PrivateRoute>
-            } />
-            
-            <Route path="/bookings" element={
-              <PrivateRoute>
-                <Bookings />
-              </PrivateRoute>
-            } />
-            
-            <Route path="/cart" element={
-              <PrivateRoute>
-                <Cart />
-              </PrivateRoute>
-            } />
-            
+            <Route
+              path="/dashboard"
+              element={
+                <PrivateRoute>
+                  <Dashboard />
+                </PrivateRoute>
+              }
+            />
+
+            <Route
+              path="/bookings"
+              element={
+                <PrivateRoute>
+                  <Bookings />
+                </PrivateRoute>
+              }
+            />
+
+            <Route
+              path="/cart"
+              element={
+                <PrivateRoute>
+                  <Cart />
+                </PrivateRoute>
+              }
+            />
+
             {/* Admin Routes */}
-            <Route path="/admin" element={
-              <AdminRoute>
-                <Admin />
-              </AdminRoute>
-            } />
-            
-            <Route path="/admin/users" element={
-              <AdminRoute>
-                <AdminUsers />
-              </AdminRoute>
-            } />
-            
-            <Route path="/admin/bookings" element={
-              <AdminRoute>
-                <AdminBookings />
-              </AdminRoute>
-            } />
-            
-            <Route path="/admin/services" element={
-              <AdminRoute>
-                <AdminServices />
-              </AdminRoute>
-            } />
-            
+            <Route
+              path="/admin"
+              element={
+                <AdminRoute>
+                  <Admin />
+                </AdminRoute>
+              }
+            />
+
+            <Route
+              path="/admin/users"
+              element={
+                <AdminRoute>
+                  <AdminUsers />
+                </AdminRoute>
+              }
+            />
+            <Route
+              path="/admin/users/:userId"
+              element={
+                <AdminRoute>
+                  <AdminUserDetails />
+                </AdminRoute>
+              }
+            />
+
+            <Route
+              path="/admin/bookings"
+              element={
+                <AdminRoute>
+                  <AdminBookings />
+                </AdminRoute>
+              }
+            />
+
+            <Route
+              path="/admin/services"
+              element={
+                <AdminRoute>
+                  <AdminServices />
+                </AdminRoute>
+              }
+            />
+
             {/* 404 Page */}
             {/* <Route path="*" element={
               <div className="text-center py-20">
@@ -117,13 +148,13 @@ function App() {
           <Route path="/register" element={null} />
           <Route path="/admin-login" element={null} />
           <Route path="/admin/*" element={null} />
-          
+
           {/* All other routes will render Footer */}
           <Route path="*" element={<Footer />} />
         </Routes>
       </div>
     </AuthProvider>
-  )
+  );
 }
 
-export default App
+export default App;
