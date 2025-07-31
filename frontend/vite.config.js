f95c7f51ac8a00f08c46ab0d447057be4623b034
Rajkumar Yogi <99001136+Rajkumaryogi@users.noreply.github.com>
2025-07-31 07:27:18 +0530

    Update vite.config.js
    change from localhost:5001 to backend link

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,8 +6,8 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        // target: 'https://car-service-production.up.railway.app',
-        target: 'http://localhost:5001',
+        target: 'https://car-service-production.up.railway.app',
+        // target: 'http://localhost:5001',
         changeOrigin: true,
         secure: false,
         rewrite: (path) => path.replace(/^\/api/, '')
